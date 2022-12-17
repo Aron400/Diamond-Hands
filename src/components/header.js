7db08949ad002772be1d499f388596714149afbe
aronCampos <aron.campos400@gmail.com>
2022-12-16 18:14:51 -0800

    Link update

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='header'>
             <h1>💎Diamond Hands💎</h1>
             <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/login'>Login</Link></li>
             </ul>
         </div>
     );
